da0e407c8a5876dad3197637bf3fad9449a8a6c8
Sasha Goloshchapov <ferrata@hey.com>
2024-09-23 13:44:35 -0500

    fix: px tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -87,7 +87,7 @@ export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="w-[650px] flex flex-col gap-4 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-4 row-start-2 items-center sm:items-start">
         <div className="w-full flex flex-row gap-4 items-center justify-center">
           <Image
             src="https://ferrata.dev/static/img/avatar.jpg"
